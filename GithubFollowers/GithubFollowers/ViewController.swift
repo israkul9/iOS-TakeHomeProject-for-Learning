db302fb074a6ce36688d93b6662cc9b4c21d534e
Israkul Hoque Tusher <israkul.hoque@bjitgroup.com>
2024-01-18 15:53:44 +0600

    Initial commit
    Project name - Github followers
    Tech Stack - iOS
    Deployment target - iOS 15

---
+++ GithubFollowers/GithubFollowers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GithubFollowers
+//
+//  Created by Israkul Tushaer-81 on 18/1/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
